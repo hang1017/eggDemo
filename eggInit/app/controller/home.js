bfff00d041cd049004851afed26d62bd81312ecc
陈书航 <chen.shuhang@iwhalecloud.com>
2021-05-08 16:17:26 +0800

    feat: mysql plugin

--- eggInit/app/controller/home.js
+++ eggInit/app/controller/home.js
@@ -4,25 +4,25 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async userListController() {
-    const { ctx } = this;
-    const data = Array.from(new Array(9)).map((_val, i) => ({
-      name: `name${i}`,
-      id: i,
-    }));
+    const { ctx, app } = this;
+    const sql = 'select * from user';
+
+    const res = await app.mysql.query(sql);
+
     ctx.body = {
-      data,
+      data: res,
       success: true,
     };
   }
 
   async userDetailController() {
-    const { ctx } = this;
+    const { ctx, app } = this;
     const { id } = ctx.request.body;
+    const sql = `select * from user where id=${id}`;
+
+    const res = await app.mysql.query(sql);
     ctx.body = {
-      data: {
-        id,
-        name: `name${id}`,
-      },
+      data: res,
       success: true,
     };
   }
